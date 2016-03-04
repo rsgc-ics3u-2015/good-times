82aa6d027138270a79f7666125a01850d382f201
Russell Gordon <rgordon@rsgc.on.ca>
2016-03-04 16:50:58 -0500

    All cities added now.
    Ready to do command line input.
    
    This program feels repetitive, right? Time to learn about functions in
    Swift (that will be after March Break).

--- Good-Times.playground/Contents.swift
+++ Good-Times.playground/Contents.swift
@@ -18,8 +18,9 @@ Begin by... making the program work perfectly, just for Victoria.
 //
 //let input : Int = 1300    // Simple case
 //let input : Int = 0       // Midnight
-//let input : Int = 2359       // A minute before midnight
-let input : Int = 59       // 00:59 AM
+//let input : Int = 2359    // A minute before midnight
+//let input : Int = 59      // 00:59 AM
+let input : Int = 2330      // 11:30 PM
 
 // Inputs
 let ottawaTime : Int = input
@@ -31,6 +32,43 @@ var victoriaTime : Int = ottawaTime - 300
 // Handle cases with negative time (times in the day before)
 if victoriaTime < 0 {
     victoriaTime = 2400 + victoriaTime
+}
+// Handle cases with time in next day
+if victoriaTime > 2359 {
+    victoriaTime = victoriaTime - 2400
+}
+
+// Edmonton
+var edmontonTime : Int = ottawaTime - 200
+// Handle cases with negative time (times in the day before)
+if edmontonTime < 0 {
+    edmontonTime = 2400 + edmontonTime
+}
+// Handle cases with time in next day
+if edmontonTime > 2359 {
+    edmontonTime = edmontonTime - 2400
+}
+
+// Winnipeg
+var winnipegTime : Int = ottawaTime - 100
+// Handle cases with negative time (times in the day before)
+if winnipegTime < 0 {
+    winnipegTime = 2400 + winnipegTime
+}
+// Handle cases with time in next day
+if winnipegTime > 2359 {
+    winnipegTime = winnipegTime - 2400
+}
+
+// Halifax
+var halifaxTime : Int = ottawaTime + 100
+// Handle cases with negative time (times in the day before)
+if halifaxTime < 0 {
+    halifaxTime = 2400 + halifaxTime
+}
+// Handle cases with time in next day
+if halifaxTime > 2359 {
+    halifaxTime = halifaxTime - 2400
 }
 
 // St. John's
@@ -55,9 +93,11 @@ if (stJohnsTime % 100 >= 60) {
     stJohnsTime = fixedTime
 }
 
-
-
 // Outputs
 print("\(ottawaTime) in Ottawa")
 print("\(victoriaTime) in Victoria")
+print("\(edmontonTime) in Edmonton")
+print("\(winnipegTime) in Winnipeg")
+print("\(ottawaTime) in Toronto") // Toronto always the same as Ottawa
+print("\(halifaxTime) in Halifax")
 print("\(stJohnsTime) in St. John's")
