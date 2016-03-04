d0a19ebb1f42bd6e43f9c35928911f4846992e3f
Russell Gordon <rgordon@rsgc.on.ca>
2016-03-04 16:25:19 -0500

    Trying another city now
    Using St. John’s since it’s the other extreme (we add an hour and a
    half to Ottawa time)

--- Good-Times.playground/Contents.swift
+++ Good-Times.playground/Contents.swift
@@ -31,6 +31,15 @@ if victoriaTime < 0 {
     victoriaTime = 2400 + victoriaTime
 }
 
+// St. John's
+var stJohnsTime : Int = ottawaTime + 130
+// Handle cases with negative time (times in the day before)
+if stJohnsTime < 0 {
+    stJohnsTime = 2400 + stJohnsTime
+}
+
+
 // Outputs
 print("\(ottawaTime) in Ottawa")
 print("\(victoriaTime) in Victoria")
+print("\(stJohnsTime) in St. John's")
